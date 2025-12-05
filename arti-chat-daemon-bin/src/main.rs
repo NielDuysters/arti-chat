093391654ff9382ad771264820c6453f668d53b6
nield <contact@ndvibes.com>
2025-12-05 01:18:32 +0100

    arti-chat-daemon-bin: Run arti-chat-daemon
    We can now run arti-chat-daemon from this binary. Temporarily we keep it simple without the logic to make it a daemon for development ease.

--- arti-chat-daemon-bin/src/main.rs
+++ arti-chat-daemon-bin/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::TRACE)
+        .init();
+
+    arti_chat_daemon::run().await?;
+    Ok(())
 }
