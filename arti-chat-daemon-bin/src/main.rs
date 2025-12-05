341f126a81f5df9c1a8fcd970fa41d3592a14770
nield <contact@ndvibes.com>
2025-12-05 02:05:33 +0100

    arti-chat-daemon-bin: Lower tracing level to DEBUG

--- arti-chat-daemon-bin/src/main.rs
+++ arti-chat-daemon-bin/src/main.rs
@@ -1,7 +1,7 @@
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::TRACE)
+        .with_max_level(tracing::Level::DEBUG)
         .init();
 
     arti_chat_daemon::run().await?;
