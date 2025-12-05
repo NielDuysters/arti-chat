7377b8090ebd7b28038221c46028edec34bef73a
nield <contact@ndvibes.com>
2025-12-05 01:08:28 +0100

    arti-chat-daemon: Add logic to make Client launch hidden service

--- arti-chat-daemon/src/client.rs
+++ arti-chat-daemon/src/client.rs
@@ -1,7 +1,76 @@
 //! The client contains the logic to launch the Tor hidden service and encapsulates
 //! services like the database, onion service,...
 
+use arti_client::config::onion_service::OnionServiceConfigBuilder;
+use crate::error;
+use futures::Stream;
+use tokio::sync::Mutex as TokioMutex;
+
+type ArtiTorClient = arti_client::TorClient<tor_rtcompat::PreferredRuntime>;
+type OnionServiceRequestStream = Box<dyn Stream<Item = tor_hsservice::RendRequest> + Send>;
+
 /// Encapsulates hidden service, database connection,...
 pub struct Client {
+    /// Arti Tor Client.
+    pub tor_client: ArtiTorClient,
+
+    /// Running hidden onion service.
     onion_service: std::sync::Arc<tor_hsservice::RunningOnionService>,
+
+    /// Request stream of onion service to handle incoming requests.
+    /// We have to store it seperately because we can't derive it after `launch_onion_service`.
+    request_stream: TokioMutex<std::pin::Pin<OnionServiceRequestStream>>,
+
+}
+
+impl Client {
+    /// Launch chat client.
+    /// Bootstrap TorClient + launch onion service + ...
+    pub async fn launch() -> Result<Self, error::ClientError> {
+        // Create Tor Client.
+        let tor_client = Self::bootstrap_tor_client().await?;
+
+        // Launch onion service.
+        let (onion_service, request_stream) = Self::launch_onion_service(&tor_client).await?;
+        let request_stream = TokioMutex::new(Box::into_pin(request_stream));
+
+        tracing::info!("ArtiChat client launched.");
+
+        Ok(Self {
+            tor_client,
+            onion_service,
+            request_stream,
+        })
+    }
+
+    async fn bootstrap_tor_client() -> Result<ArtiTorClient, error::ClientError> {
+        let config = arti_client::TorClientConfig::default();
+        let client = arti_client::TorClient::create_bootstrapped(config).await?;
+
+        tracing::info!("Tor Client bootstrapped.");
+
+        Ok(client)
+    }
+    
+    async fn launch_onion_service(client: &ArtiTorClient)
+        -> Result<(
+            std::sync::Arc<tor_hsservice::RunningOnionService>,
+            OnionServiceRequestStream,
+        ), error::ClientError> {
+            let config = OnionServiceConfigBuilder::default()
+            .nickname("arti-chat-service".parse()?)
+            .build()?;
+
+        let (onion_service, request_stream) = match client.launch_onion_service(config)? {
+            Some(v) => v,
+            None => {
+                return Err(error::ClientError::OnionServiceDisabled);
+            }
+        };
+        let request_stream = Box::new(request_stream);
+        
+        tracing::info!("Onion service launched.");
+    
+        Ok((onion_service, request_stream))
+    }
 }
--- arti-chat-daemon/src/error.rs
+++ arti-chat-daemon/src/error.rs
@@ -10,3 +10,22 @@ pub enum DaemonError {
     RunError(String),
 }
 
+/// Errors related to client.
+#[derive(Error, Clone, Debug)]
+pub enum ClientError {
+    /// Error in Arti TorClient.
+    #[error("Tor Client error: {0}")]
+    TorClientError(#[from] arti_client::Error),
+
+    /// Specified onion service is disabled in config.
+    #[error("Specified onion service is disabled in config")]
+    OnionServiceDisabled,
+   
+    /// Invalid nickname for onion service.
+    #[error("Invalid nickname for onion service: {0}")]
+    OnionServiceInvalidNickname(#[from] tor_persist::hsnickname::InvalidNickname),
+    
+    /// Failed to build Arti config.
+    #[error("Failed to build Arti config: {0}")]
+    ArtiConfigBuildError(#[from] arti_client::config::ConfigBuildError),
+}
