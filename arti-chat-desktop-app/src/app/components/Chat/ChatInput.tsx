91c705f1d26d596e8778c2ebbf6c29bb0e01c9ec
nield <contact@ndvibes.com>
2025-12-16 22:02:24 +0100

    arti-chat-desktop-app: Allow multi-line input + send message by Enter-press

--- arti-chat-desktop-app/src/app/components/Chat/ChatInput.tsx
+++ arti-chat-desktop-app/src/app/components/Chat/ChatInput.tsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./ChatInput.scss";
 
 export default function ChatInput({ sendMessage }) {
     const [text, setText] = useState("");
+    const textInputRef = useRef(null);
 
+    // Send message.
     const handleSend = async () => {
         if  (text.trim().length === 0) {
             return;
@@ -13,20 +15,42 @@ export default function ChatInput({ sendMessage }) {
         setText("");
     }
 
+    // Send at Enter-key press.
+    const handleKeydown = (e) => {
+        if (e.key === "Enter" && !e.shiftKey) {
+            e.preventDefault();
+            handleSend();
+        }
+    }
+
+    // Grow textarea height if multiple lines.
+    const handleKeyup = (e) => {
+        if (/\r|\n/.exec(text)) {
+            textInputRef.current.style.height = "80px";
+        } else {
+            textInputRef.current.style.height = "40px";
+        }
+    }
+
     return (
         <div className="chat-input">
-            <textarea
-                className="chat-input__text"
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-                placeholder="Type your message..."
-            ></textarea>
-            <img
-                src="/src/assets/send.png"
-                alt="Send message"
-                className="chat-input__send-btn"
-                onClick={handleSend}
-            />
+            <div className="chat-input__inner">
+                <textarea
+                    ref={textInputRef}
+                    className="chat-input__text"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    onKeyDown={handleKeydown}
+                    onKeyUp={handleKeyup}
+                    placeholder="Type your message..."
+                ></textarea>
+                <img
+                    src="/src/assets/send.png"
+                    alt="Send message"
+                    className="chat-input__send-btn"
+                    onClick={handleSend}
+                />
+            </div>
         </div>
     );
 }
