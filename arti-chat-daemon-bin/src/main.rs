34bb9bf6aab59f26a0c89767dd3d8a28e797a934
nield <contact@ndvibes.com>
2025-12-17 16:27:10 +0100

    arti-chat-daemon-bin: Run arti-chat-daemon as background task

--- arti-chat-daemon-bin/src/main.rs
+++ arti-chat-daemon-bin/src/main.rs
@@ -1,9 +1,23 @@
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
+use daemonize::Daemonize;
+use tokio::runtime::Runtime;
+
+fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::DEBUG)
         .init();
+    
+    let daemon = Daemonize::new()
+        .pid_file("/tmp/arti-chat-daemon.pid")
+        .working_directory("/")
+        .umask(0o027);
+    daemon.start().expect("Failed to daemonize");
 
-    arti_chat_daemon::run().await?;
+    let rt = Runtime::new()?;
+    rt.block_on(async {
+        if let Err(e) = arti_chat_daemon::run().await {
+            tracing::error!("Daemon runtime error: {:?}", e);
+        }
+    });
+
     Ok(())
 }
