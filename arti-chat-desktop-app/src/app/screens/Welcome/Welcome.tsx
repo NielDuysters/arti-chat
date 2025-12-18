b50f7effddcb1a0d88d4ed2f35f143d106178424
nield <contact@ndvibes.com>
2025-12-18 02:44:41 +0100

    arti-chat-desktop-app: typo

--- arti-chat-desktop-app/src/app/screens/Welcome/Welcome.tsx
+++ arti-chat-desktop-app/src/app/screens/Welcome/Welcome.tsx
@@ -4,8 +4,8 @@ import "./Welcome.scss";
 export default function Welcome()  {
     return (
         <div className="screen screen--welcome">
-            <h2>Welcome to ArtiChat</h2>
-            <h4>You can talk freely now.</h4>
+            <h2>Welcome to Arti Chat</h2>
+            <h4>You can talk freely.</h4>
             <a className="screen--welcome__donate-btn" href="https://donate.torproject.org" target="_blank">
                 <span>Donate to Tor</span>
             </a>
