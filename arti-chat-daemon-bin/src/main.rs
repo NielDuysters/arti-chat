39b1522d4c59da063d476c5c91bcb5eb395d27a3
nield <contact@ndvibes.com>
2025-12-28 06:46:53 +0100

    arti-chat-daemon-bin: Remove daemonize logic

--- arti-chat-daemon-bin/src/main.rs
+++ arti-chat-daemon-bin/src/main.rs
@@ -1,23 +1,9 @@
-use daemonize::Daemonize;
 use tokio::runtime::Runtime;
 
 fn main() -> anyhow::Result<()> {
-    let args: Vec<String> = std::env::args().collect();
-    let manual_daemonize = args.get(1).map(String::as_str) == Some("run");
-
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::DEBUG)
         .init();
-
-    if manual_daemonize {
-        let daemon = Daemonize::new()
-            .pid_file("/tmp/arti-chat-daemon.pid")
-            .working_directory("/")
-            .umask(0o027);
-
-        daemon.start()
-            .map_err(|e| anyhow::anyhow!("Failed to daemonize: {}", e))?;
-    }
 
     let rt = Runtime::new()?;
     rt.block_on(async {
