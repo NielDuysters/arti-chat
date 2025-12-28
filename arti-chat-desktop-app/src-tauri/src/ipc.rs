0d07cfd1fc4f87990e521f463d1319ccb0887969
nield <contact@ndvibes.com>
2025-12-28 06:47:18 +0100

    arti-chat-desktop-app: Fix launch_daemon for MacOS

--- arti-chat-desktop-app/src-tauri/src/ipc.rs
+++ arti-chat-desktop-app/src-tauri/src/ipc.rs
@@ -28,16 +28,27 @@ pub async fn launch_daemon() -> anyhow::Result<()> {
         return Ok(());
     }
 
-    let launch_status = if cfg!(target_os = "macos") {
-        std::process::Command::new("launchctl").args(["start", "com.arti-chat.daemon"]).spawn()
+    let status = if cfg!(target_os = "macos") {
+        let uid = nix::unistd::Uid::current();
+
+        std::process::Command::new("launchctl")
+            .args([
+                "kickstart",
+                "-k",
+                &format!("gui/{}/com.arti-chat.daemon", uid.as_raw()),
+            ])
+            .status()
     } else if cfg!(target_os = "linux") {
-        std::process::Command::new("systemctl").args(["--user", "start", "com.arti-chat.daemon.service"]).spawn()
+        std::process::Command::new("systemctl")
+            .args(["--user", "start", "com.arti-chat.daemon.service"])
+            .status()
     } else {
         anyhow::bail!(error::DesktopUiError::UnsupportedOperatingSystem);
-    };
+    }?;
 
-    match launch_status {
-        Ok(_) => return Ok(()),
-        Err(_) => anyhow::bail!(error::DesktopUiError::DaemonStartFailure),
+    if !status.success() {
+        anyhow::bail!(error::DesktopUiError::DaemonStartFailure);
     }
+
+    Ok(())
 }
